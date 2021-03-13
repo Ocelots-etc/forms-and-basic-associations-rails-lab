5ee91ffb6512dfa91c291734505035be298967a5
Ocelots-etc <catherinedgomez@gmail.com>
2021-03-13 17:54:52 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,13 +9,22 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    # @song.artist_name
+    # @song.genres.build(name: "")
+    # @song.notes.build(name:)
+    # Build 3 "empty" notes that are automatically set up with the correct
+    # parameter names to be used by accepts_nested_attributes_for
+    # See:
+    # https://api.rubyonrails.org/v5.2.3/classes/ActionView/Helpers/FormHelper.html#method-i-fields_for
+    3.times { @song.notes.build }
   end
 
   def create
-    @song = Song.new(song_params)
+    artist = Artist.find_or_create_by(name: song_params[:artist_name])
+    @song = artist.songs.build(song_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to songs_path
     else
       render :new
     end
@@ -47,7 +56,62 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
+
+
+
+# class SongsController < ApplicationController
+#   def index
+#     @songs = Song.all
+#   end
+
+#   def show
+#     @song = Song.find(params[:id])
+#   end
+
+#   def new
+#     @song = Song.new
+#   end
+
+#   def create
+#     @song = Song.new(song_params)
+
+#     if @song.save
+#       redirect_to @song
+#     else
+#       render :new
+#     end
+#   end
+
+#   def edit
+#     @song = Song.find(params[:id])
+#   end
+
+#   def update
+#     @song = Song.find(params[:id])
+
+#     @song.update(song_params)
+
+#     if @song.save
+#       redirect_to @song
+#     else
+#       render :edit
+#     end
+#   end
+
+#   def destroy
+#     @song = Song.find(params[:id])
+#     @song.destroy
+#     flash[:notice] = "Song deleted."
+#     redirect_to songs_path
+#   end
+
+#   private
+
+#   def song_params
+#     params.require(:song).permit(:title)
+#   end
+# end
 
